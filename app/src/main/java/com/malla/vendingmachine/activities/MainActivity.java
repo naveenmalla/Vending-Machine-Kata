db7acb120a9bae1e22320371120ff4003b8d1c6c
naveenmalla <mallanaveen@gmail.com>
2018-04-02 19:44:17 -0700

    Initial commit

---
+++ app/src/main/java/com/malla/vendingmachine/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.malla.vendingmachine.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.malla.vendingmachine.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
